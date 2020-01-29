aa39d2ff4e6534d2506ae98c2f54259172428aaf
Nargiz <nargiz1397@mail.ru>
2020-01-29 23:24:33 +0300

    validation tests for all_scripts_description

--- app/models/all_scripts_description.rb
+++ app/models/all_scripts_description.rb
@@ -1,5 +1,5 @@
 class AllScriptsDescription < ApplicationRecord
-	validates :name_script, presence: true
+	validates :name_script, presence: true, uniqueness: true
 	validates :steps, presence: true
 
 end
--- spec/models/all_scripts_description_spec.rb
+++ spec/models/all_scripts_description_spec.rb
@@ -4,9 +4,25 @@ RSpec.describe AllScriptsDescription, type: :model do
  # pending "add some examples to (or delete) #{__FILE__}"
  
  context "validation tests" do
+ 	before :each do
+ 		@script_description = AllScriptsDescription.new(name_script: "QWERTY", steps: "s1,s2,s3,s4,s5")
+ 	end
  	it "should be valid" do
- 		script_description = AllScriptsDescription.new(name_script: "QWERTY", steps: "s1,s2,s3,s4,s5")
- 		expect(script_description.valid?).to eq(true)
+ 		expect(@script_description.valid?).to eq(true)
+ 	end
+ 	it "name_script should be present" do
+ 		@script_description.name_script = "     "
+ 		expect(@script_description.valid?).to eq(false)
+ 	end
+ 	it "steps shold be present" do
+ 		@script_description.steps = "     "
+ 		expect(@script_description.valid?).to eq(false)
+ 	end
+ 	it "name_script should be unique" do
+ 		duplicate_script = @script_description.dup
+ 		duplicate_script.name_script = @script_description.name_script.upcase
+ 		@script_description.save
+ 		expect(duplicate_script.valid?).to eq(false)
  	end
  end
 end
