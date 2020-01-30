068430ea2441d1641c425947aeac4da684c028ca
Nargiz <nargiz1397@mail.ru>
2020-01-30 11:34:39 +0300

    realized validates tests for Step model

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,2 +1,6 @@
 class Step < ApplicationRecord
+	validates :name_step, presence: true
+	validates :name_script, presence: true
+	validates :status, presence: true
+	validates :rollbacks, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 end
--- spec/models/step_spec.rb
+++ spec/models/step_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Step, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # pending "add some examples to (or delete) #{__FILE__}"
+  context "validation tests" do
+ 	before :each do
+ 		@step = Step.new(name_step: "StepQWERTY", name_script: "QWERTY", status: "Finish", rollbacks: 0)
+ 	end
+ 	it "should be valid" do
+ 		expect(@step.valid?).to eq(true)
+ 	end
+ 	it "name_step should be present" do
+ 		@step.name_step = "     "
+ 		expect(@step.valid?).to eq(false)
+ 	end
+
+ 	it "name_script should be present" do
+ 		@step.name_script = "     "
+ 		expect(@step.valid?).to eq(false)
+ 	end
+ 	
+ 	it "rollbacks should be integer, negative example" do
+ 		@step.rollbacks = "dfg"
+ 		expect(@step.valid?).to eq(false)
+ 	end
+	it "rollbacks should be integer, positive example" do
+ 		@step.rollbacks = 5
+ 		expect(@step.valid?).to eq(true)
+ 	end
+ 	
+ 	it "rollbacks should be greater or equal 0, positive example" do
+ 		@step.rollbacks = 5
+ 		expect(@step.valid?).to eq(true)
+ 	end
+ 	
+ 	it "rollbacks should be greater or equal 0, negative example" do
+ 		@step.rollbacks = -3
+ 		expect(@step.valid?).to eq(false)
+ 	end
+ 	it "status should be present" do
+ 		@step.status = "    "
+ 		expect(@step.valid?).to eq(false)
+ 	end
+  end
 end
