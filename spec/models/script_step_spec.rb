564d74713d8ee1fc147053959137ab4737ff383a
Nargiz <nargiz1397@mail.ru>
2020-01-30 11:36:48 +0300

    delete uniqueness validates in ScriptStep model, bkz need to delegate this validate to DataBase

--- spec/models/script_step_spec.rb
+++ spec/models/script_step_spec.rb
@@ -3,36 +3,20 @@ require 'rails_helper'
 RSpec.describe ScriptStep, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   context "validation test" do
-	before :each do
- 		@script_step = ScriptStep.new(script_name: "QWERTY",name_step:"stepQwerty")
- 	end
- 	it "should be valid" do
- 		expect(@script_step.valid?).to eq(true)
-
- 	end
-	it "script_name should be present" do
- 		@script_step.script_name = "  "
- 		expect(@script_step.valid?).to eq(false)
- 	end
-
-	it "name_step should be present" do
-		@script_step.name_step = "     "
-		expect(@script_step.valid?).to eq(false) 
- 	end
-
-  	it "script_name should be unique " do
-  		duplicate_script_step = @script_step.dup
- 		duplicate_script_step.script_name = @script_step.script_name.upcase
- 		@script_step.save
- 		expect(duplicate_script_step.valid?).to eq(false)
-  	end
-  
- 	it "name_step should be unique " do
-  		duplicate_script_step = @script_step.dup
- 		duplicate_script_step.name_step = @script_step.name_step.upcase
- 		@script_step.save
- 		expect(duplicate_script_step.valid?).to eq(false)
-  	end
-
+    before :each do
+      @script_step = ScriptStep.new(script_name: "QWERTY",name_step:"stepQwerty")
+ 	  end
+ 	  it "should be valid" do
+ 		 expect(@script_step.valid?).to eq(true)
+ 	  end
+    it "script_name should be present" do
+      @script_step.script_name = "  "
+      expect(@script_step.valid?).to eq(false)
+ 	  end
+    it "name_step should be present" do
+      @script_step.name_step = "     "
+      expect(@script_step.valid?).to eq(false) 
+    end
+ 	  
   end
 end
