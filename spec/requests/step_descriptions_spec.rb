e4d0a62fa116a74d7e733b48430150331de68c23
Nargiz <nargiz1397@mail.ru>
2020-01-30 11:39:04 +0300

    comment some unnecessary code

--- spec/requests/step_descriptions_spec.rb
+++ spec/requests/step_descriptions_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "StepDescriptions", type: :request do
-  describe "GET /step_descriptions" do
-    it "works! (now write some real specs)" do
-      get step_descriptions_path
-      expect(response).to have_http_status(200)
-    end
-  end
-end
+# RSpec.describe "StepDescriptions", type: :request do
+#   describe "GET /step_descriptions" do
+#     it "works! (now write some real specs)" do
+#       get step_descriptions_path
+#       expect(response).to have_http_status(200)
+#     end
+#   end
+# end
