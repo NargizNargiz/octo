93c21ed2ee7eda54faa5cb1de6065a7aaa9a478b
Nargiz <nargiz1397@mail.ru>
2020-03-04 19:16:17 +0300

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'all_scripts_descriptions/show'
-  get 'all_scripts_descriptions/new'
-  get 'all_scripts_descriptions/create'
-  get 'sttep_description/index'
-  get 'sttep_description/show'
-  get 'sttep_description/new'
-  get 'sttep_description/edit'
-  get 'sttep_description/create'
-  get 'sttep_description/update'
-  get 'sttep_description/destroy'
-  resources :step_descriptions
-  resources :sttep_descriptions
-  
+  resources :all_scripts_descriptions  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
