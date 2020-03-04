2dd1854c039a1039a7c1443ef28dad82e3a3917f
Nargiz <nargiz1397@mail.ru>
2020-03-04 21:11:52 +0300

    corrected create method

--- app/controllers/all_scripts_descriptions_controller.rb
+++ app/controllers/all_scripts_descriptions_controller.rb
@@ -5,12 +5,19 @@ class AllScriptsDescriptionsController < ApplicationController
 
   def create
   	# render plain: params[:all_scripts_description].inspect
-  	@all_scripts_description = AllScriptsDescription.new(params[:all_scripts_description])
+  	@all_scripts_description = AllScriptsDescription.new(all_scripts_description_params)
 	@all_scripts_description.save
 	redirect_to @all_scripts_description
   end
   def index
   	@all_scripts_descriptions = AllScriptsDescription.all
   end
+  def show
+  	@all_scripts_description = AllScriptsDescription.find(params[:id])
+	end
+  private
+ 	def all_scripts_description_params
+    	params.require(:all_scripts_description).permit(:name_script, :steps)
+  	end
 
 end
