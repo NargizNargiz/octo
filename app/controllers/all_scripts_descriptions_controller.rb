0b66f5b27ad12e9d9730c6d59a9aa7d6610c401f
Nargiz <nargiz1397@mail.ru>
2020-03-04 20:19:47 +0300

    add index and create methods

--- app/controllers/all_scripts_descriptions_controller.rb
+++ app/controllers/all_scripts_descriptions_controller.rb
@@ -1,13 +1,16 @@
 class AllScriptsDescriptionsController < ApplicationController
-  def show
-
-  end
-
   def new
 	@all_scripts_description = AllScriptsDescription.new  	
   end
 
   def create
-  
+  	# render plain: params[:all_scripts_description].inspect
+  	@all_scripts_description = AllScriptsDescription.new(params[:all_scripts_description])
+	@all_scripts_description.save
+	redirect_to @all_scripts_description
   end
+  def index
+  	@all_scripts_descriptions = AllScriptsDescription.all
+  end
+
 end
