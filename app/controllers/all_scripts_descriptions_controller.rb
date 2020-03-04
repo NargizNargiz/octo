87b8b05bbfee9e9f1f6e51c3ef6ae1cfd25ebb8b
Nargiz <nargiz1397@mail.ru>
2020-03-04 19:28:35 +0300

    add new method

--- app/controllers/all_scripts_descriptions_controller.rb
+++ app/controllers/all_scripts_descriptions_controller.rb
@@ -1,10 +1,13 @@
 class AllScriptsDescriptionsController < ApplicationController
   def show
+
   end
 
   def new
+	@all_scripts_description = AllScriptsDescription.new  	
   end
 
   def create
+  
   end
 end
