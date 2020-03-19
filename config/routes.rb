5f9f534c5f0d16a6857e36f90f92023fa6bdde87
Nargiz <nargiz1397@mail.ru>
2020-03-19 11:20:58 +0300

    добавила  в routes exec_script

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :all_scripts_descriptions
-	
+	resources :exec_scripts
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
