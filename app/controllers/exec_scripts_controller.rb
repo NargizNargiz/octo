a31b420c1167977b75bde4133ca57f90996c2404
Nargiz <nargiz1397@mail.ru>
2020-03-19 11:18:59 +0300

    добавила контроллер и вьюхи exec_script

---
+++ app/controllers/exec_scripts_controller.rb
@@ -0,0 +1,38 @@
+class ExecScriptsController < ApplicationController
+  def index
+  	@exec_scripts = ExecScript.all
+  end
+
+  def show
+    @exec_script = ExecScript.find(params[:id])
+	end
+
+  def new
+    @exec_script = ExecScript.new  	
+  end
+
+  def edit
+  end
+  
+  def create
+  	# render plain: params[:all_scripts_description].inspect
+  	@exec_script = ExecScript.new(exec_script_params)
+
+    respond_to do |format|
+      if @exec_script.save
+        format.html { redirect_to @exec_script, notice: 'exec_script was successfully created.' }
+        format.json { render :show, status: :created, location: @exec_script }
+      else
+        format.html { render :new }
+        format.json { render json: @exec_script.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+ 	  def exec_script_params
+    	params.require(:exec_script).permit(:script_name, :rollbacks, :limit_errors, :success)
+  	end
+
+
+end
