c0b88e0e25647bbeba06ae8afe36726ea991f75a
Nargiz <nargiz1397@mail.ru>
2020-04-15 14:05:47 +0300

    добавленое регулярное выражение для валидации списка шагов

--- app/models/all_scripts_description.rb
+++ app/models/all_scripts_description.rb
@@ -1,5 +1,6 @@
 class AllScriptsDescription < ApplicationRecord
 	validates :name_script, presence: true, uniqueness: true
-	validates :steps, presence: true
+	# validates :name_script, presence: true	
+	validates :steps, presence: true, format: {with: /\A([A-Za-z ]+)(\1)*\Z/, message: "Введите шаги через пробел" }
 
 end
