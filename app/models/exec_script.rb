537305d077e3d30e35c729aef14f05a4c3445310
Nargiz <nargiz1397@mail.ru>
2020-04-15 14:08:54 +0300

    добавленое обновления-сохранения вложенных атрибутов

--- app/models/exec_script.rb
+++ app/models/exec_script.rb
@@ -1,4 +1,5 @@
 class ExecScript < ApplicationRecord
+	# validates :script_name, presence: true, uniqueness: true
 	validates :script_name, presence: true, uniqueness: true
 	validates :rollbacks, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 	validates :limit_errors, numericality: {only_integer: true, greater_than_or_equal_to: 0}
--- app/models/script_step.rb
+++ app/models/script_step.rb
@@ -4,4 +4,5 @@ class ScriptStep < ApplicationRecord
 	has_many  :steps, dependent: :destroy
 	belongs_to :exec_script
 	belongs_to :all_scripts_description
+	accepts_nested_attributes_for :all_scripts_description, :exec_script
 end
