5497450c31c564ba8119a5dbe87b774fd6f511c9
Nargiz <nargiz1397@mail.ru>
2020-04-15 14:12:08 +0300

    изменена валидация

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,8 +1,7 @@
 class Step < ApplicationRecord
 	validates :name_step, presence: true
 	validates :name_script, presence: true
-	validates :status, presence: true
+	validates :status, presence: true, numericality: {only_integer: true}
 	validates :rollbacks, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 	belongs_to :script_step
-
 end
