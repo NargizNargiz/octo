cbf13b2e0cec351b1e33f2fac28487df397c7917
Nargiz <nargiz1397@mail.ru>
2020-04-28 08:46:02 +0300

    изменена связь

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,5 +3,6 @@ class Step < ApplicationRecord
 	validates :name_script, presence: true
 	validates :status, presence: true, numericality: {only_integer: true}
 	validates :rollbacks, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
-	belongs_to :script_step
+	belongs_to :exec_script
+	accepts_nested_attributes_for  :exec_script
 end
