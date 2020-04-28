eaf89ea2cf00d245d341a1a656e7c1ae35cf1c98
Nargiz <nargiz1397@mail.ru>
2020-04-28 08:43:28 +0300

    новое название скриптов в seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times{AllScriptsDescription.create(name_script: Faker::Nation.unique.language, steps:  Array.new(rand(2..4)) { Faker::Verb.base }.join(" ") ) }
+10.times{AllScriptsDescription.create(name_script: "script" + Faker::Nation.unique.language , steps:  Array.new(rand(2..4)) { Faker::Verb.base }.join(" ") ) }
 all_scripts_descs = AllScriptsDescription.pluck(:name_script)
 
 7.times do |i|
-	all_scripts_descs = AllScriptsDescription.find(rand(1..7))
+	all_scripts_descs = AllScriptsDescription.find(rand(1..AllScriptsDescription.all.count))
 	exec_script = ExecScript.create(script_name: all_scripts_descs.name_script,
 					rollbacks: 0,
 					limit_errors: rand(2..9),
@@ -17,7 +17,7 @@ all_scripts_descs = AllScriptsDescription.pluck(:name_script)
 	script_step = ScriptStep.create(script_name: exec_script.script_name, name_step: "step", exec_script: exec_script, all_scripts_description: all_scripts_descs)
 	
 	for step in all_scripts_descs.steps.split(" ") do
-		step_obj = Step.create(name_step: step, name_script: exec_script.script_name, status:0, rollbacks:0, script_step: script_step)
+		step_obj = Step.create(name_step: step, name_script: exec_script.script_name, status:0, rollbacks:0, exec_script: exec_script)
       	script_step.steps << step_obj
 	end
 
