85a1f4214650c47e5d66b34b25ea9c4ae87e1003
Nargiz <nargiz1397@mail.ru>
2020-05-07 19:56:47 +0300

    delete reduntant function

--- app/models/exec_script.rb
+++ app/models/exec_script.rb
@@ -3,5 +3,5 @@ class ExecScript < ApplicationRecord
 	validates :script_name, presence: true, uniqueness: true
 	validates :rollbacks, numericality: {only_integer: true, greater_than_or_equal_to: 0} 
 	validates :limit_errors, numericality: {only_integer: true, greater_than_or_equal_to: 0}
-	validates :success, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 2}	
+	validates :success, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 2}
 end
