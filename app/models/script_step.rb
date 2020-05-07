cee337ef55242360511b43fa4e3baa3c36486302
Nargiz <nargiz1397@mail.ru>
2020-05-07 20:53:38 +0300

    добавлена зависимоисть по удалени.

--- app/models/script_step.rb
+++ app/models/script_step.rb
@@ -2,7 +2,7 @@ class ScriptStep < ApplicationRecord
 	validates :script_name, presence: true
 	validates :name_step, presence: true
 	has_many  :steps, dependent: :destroy
-	belongs_to :exec_script
+	belongs_to :exec_script, dependent: :destroy
 	belongs_to :all_scripts_description
 	accepts_nested_attributes_for :all_scripts_description, :exec_script
 end
