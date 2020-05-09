89be02a397ccd07cb48d4fd1e870c9c98d931579
Nargiz <nargiz1397@mail.ru>
2020-05-09 20:49:37 +0300

    сид для тестирования скрипта

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-10.times{AllScriptsDescription.create(name_script: "script" + Faker::Nation.unique.language , steps:  Array.new(rand(2..4)) { Faker::Verb.base }.join(" ") ) }
-all_scripts_descs = AllScriptsDescription.pluck(:name_script)
+# 10.times{AllScriptsDescription.create(name_script: "script" + Faker::Nation.unique.language , steps:  Array.new(rand(2..4)) { Faker::Verb.base }.join(" ") ) }
+# all_scripts_descs = AllScriptsDescription.pluck(:name_script)ion.create(name_script: "script" + Faker::Nation.unique.language , steps:  Array.new(rand(2..4)) { Faker::Verb.base }.join(" ") ) }
+# all_scripts_descs = AllScriptsDescription.pluck(:name_script)
+#
+# 7.times do |i|
+# 	all_scripts_descs = AllScriptsDescription.find(rand(1..AllScriptsDescription.all.count))
+# 	exec_script = ExecScript.create(script_name: all_scripts_descs.name_script,
+# 					rollbacks: 0,
 
-7.times do |i|
-	all_scripts_descs = AllScriptsDescription.find(rand(1..AllScriptsDescription.all.count))
-	exec_script = ExecScript.create(script_name: all_scripts_descs.name_script,
-					rollbacks: 0,
-					limit_errors: rand(2..9),
-					success:0)
-	script_step = ScriptStep.create(script_name: exec_script.script_name, name_step: "step", exec_script: exec_script, all_scripts_description: all_scripts_descs)
-	
-	for step in all_scripts_descs.steps.split(" ") do
-		step_obj = Step.create(name_step: step, name_script: exec_script.script_name, status:0, rollbacks:0, exec_script: exec_script)
-      	script_step.steps << step_obj
-	end
+#
+# 7.times do |i|
+# 	all_scripts_descs = AllScriptsDescription.find(rand(1..AllScriptsDescription.all.count))
+# 	exec_script = ExecScript.create(script_name: all_scripts_descs.name_script,
+# 					rollbacks: 0,
+# 					limit_errors: rand(2..9),
+# 					success:0)
+# 	script_step = ScriptStep.create(script_name: exec_script.script_name, name_step: "step", exec_script: exec_script, all_scripts_description: all_scripts_descs)
+#
+# 	for step in all_scripts_descs.steps.split(" ") do
+# 		step_obj = Step.create(name_step: step, name_script: exec_script.script_name, status:0, rollbacks:0, exec_script: exec_script)
+#       	script_step.steps << step_obj
+#   end
+# end
 
-end
+
+steps = "set_connection check_group add_ssh_key  add_user_to_group add_ssh_key"
+
+AllScriptsDescription.create(name_script: "add_user" , steps:  steps )
 
 
 
